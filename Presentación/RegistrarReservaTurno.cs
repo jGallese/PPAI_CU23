3bb287c31afa33c6fd2ef670c750b1f7b6cbc3ab
jGallese <87546414+jGallese@users.noreply.github.com>
2022-06-15 17:46:16 -0300

    Add project files.

---
+++ Presentación/RegistrarReservaTurno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CU_23
+{
+    public partial class RegistrarReservaTurno : Form
+    {
+        public RegistrarReservaTurno()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
